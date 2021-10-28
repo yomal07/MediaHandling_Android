681846158dc3552b54d7acb7d44e318b066df615
Yomal Bandara <yomalb07@gmail.com>
2021-10-28 14:21:06 +0530

    Implemented the UI

---
+++ app/src/main/java/com/example/mediahandling/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.mediahandling;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
