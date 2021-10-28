7f721b7074bfcd0616334c7a635504ee58ed72e8
Yomal Bandara <yomalb07@gmail.com>
2021-10-28 14:27:28 +0530

    implemented the onclick methods

--- app/src/main/java/com/example/mediahandling/MainActivity.java
+++ app/src/main/java/com/example/mediahandling/MainActivity.java
@@ -7,10 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void play(View view){
+        if(mediaPlayer ==  null){
+            mediaPlayer = MediaPlayer.create(getApplicationContext(),R.raw.song);
+            mediaPlayer.start();
+        }else{
+            mediaPlayer.start();
+        }
+    }
+    public void pause(View view){
+        if(mediaPlayer != null){
+            mediaPlayer.pause();
+        }
+    }
+    public void stop(View view){
+        mediaPlayer.release();
+        mediaPlayer = null;
     }
 
 
